2ab051457886f1610338d515ccaad92dc8c85943
Smirnov Nikita <nikitaloko@mail.ru>
2025-03-08 17:27:17 +0300

    Первый раз отправляю на проверку

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Проектная работа Mesto
 
+Ссылка на мой репозиторий: https://github.com/nikita-sm/mesto-project-ff
+
+Ссылка на мой репозиторий Пятого спринта с расширением: https://github.com/nikita-sm/mesto-project-ff.git
+
+Ссылка на документацию (403 ошибка): https://docs.github.com/en/get-started/getting-started-with-git/managing-remote-repositories?platform=windows
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,38 @@
 // @todo: Темплейт карточки
+const template = document.querySelector("#card-template").content;
+const clone = template.cloneNode(true);
+
 
 // @todo: DOM узлы
+const list = document.querySelector(".places__list");
+
 
 // @todo: Функция создания карточки
+function createCard(item, fn){
+    const clone = template.cloneNode(true);
+    const li = clone.querySelector(".places__item");
+    let imgCard = li.querySelector(".card__image");
+    let titleCard = li.querySelector(".card__title");
+    let deleteButton = li.querySelector(".card__delete-button");
+    deleteButton.addEventListener("click", function(){
+       deleteCard(deleteButton);
+    })
+    const name = item.name;
+    const link = item.link;
+    imgCard.src = link;
+    imgCard.alt = "Изображение";
+    imgCard.setAttribute("loading", "lazy");
+    titleCard.textContent = name;
+    list.append(clone);
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(deleteButton){
+    let card = deleteButton.closest(".places__item");
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach((item) => {
+    createCard(item, deleteCard);
+})
